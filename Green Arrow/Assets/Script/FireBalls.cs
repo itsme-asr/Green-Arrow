5b331c597f56790dbb2fc0fad7052fce52f7907f
A.S.Rawat <adityasrwt5@gmail.com>
2023-02-13 20:36:44 +0530

    Update

--- Green Arrow/Assets/Script/FireBalls.cs
+++ Green Arrow/Assets/Script/FireBalls.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class FireBalls : MonoBehaviour
 {
+    [SerializeField] GameObject deathEffectprefab;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
+            GameObject death = Instantiate(deathEffectprefab, transform.position, Quaternion.identity);
             Destroy(other.gameObject, .1f);
+            Destroy(death, .3f);
             Destroy(gameObject);
         }
     }
